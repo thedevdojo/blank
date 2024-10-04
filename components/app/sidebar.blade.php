c5b243bf2c805e17b9c22fe2a654a7150bd1d8f8
Tony Lea <tnylea@gmail.com>
2024-10-04 17:58:41 -0400

    Adding a few updates to blank theme

--- components/app/sidebar.blade.php
+++ components/app/sidebar.blade.php
@@ -1,14 +1,14 @@
-<aside x-data="{ sidebarOpen: false }" :class="{ 'w-screen' : sidebarOpen, 'pointer-events-none lg:pointer-events-auto' : !sidebarOpen }"  @open-sidebar.window="sidebarOpen = true" class="fixed z-40 md:z-50 w-64 lg:w-auto">
+<aside x-data="{ sidebarOpen: false }" :class="{ 'w-screen' : sidebarOpen, 'pointer-events-none lg:pointer-events-auto' : !sidebarOpen }"  @open-sidebar.window="sidebarOpen = true" class="fixed z-40 w-64 md:z-50 lg:w-auto">
     {{-- Backdrop for mobile --}}
     <div x-show="sidebarOpen" @click="sidebarOpen=false" class="fixed top-0 right-0 z-40 w-screen h-screen duration-300 ease-out bg-black/20 dark:bg-white/10" x-cloak></div>
 
     <div :class="{ '-translate-x-full': !sidebarOpen }"
-        class="relative flex flex-col justify-between overflow-scroll w-64 z-50 h-screen bg-white border-r border-black scrollbar-hide overflow-x-hidden -translate-x-full lg:translate-x-0">
+        class="relative z-50 flex flex-col justify-between w-64 h-screen overflow-scroll overflow-x-hidden -translate-x-full bg-white border-r border-black scrollbar-hide lg:translate-x-0">
         <div class="flex flex-col items-start p-6 sm:p-7">
-            <a href="/dashboard">
-                <x-logo class="mb-6 w-auto h-7"></x-logo>
+            <a href="/">
+                <x-logo class="w-auto mb-6 h-7"></x-logo>
             </a>
-            <nav class="flex flex-col -mx-1 space-y-2 w-full">
+            <nav class="flex flex-col w-full -mx-1 space-y-2">
                 <x-app.sidebar-link href="/dashboard" active="true">Menu Item 1</x-app.sidebar-link>
                 <x-app.sidebar-link onclick="sidebarLinkDemo(event)">Menu Item 2</x-app.sidebar-link>
                 <x-app.sidebar-dropdown text="Menu Dropdown" icon="phosphor-stack" id="example_dropdown" :active="false" :open="false">
--- components/marketing/header.blade.php
+++ components/marketing/header.blade.php
@@ -1,17 +1,17 @@
 <section x-data="{ mobileNavOpen: false }" class="overflow-hidden">
-    <div class="flex justify-between items-center p-5 mx-auto max-w-6xl bg-white">
+    <div class="flex items-center justify-between max-w-6xl p-5 mx-auto bg-white">
         <div class="w-auto">
             <div class="flex flex-wrap items-center">
-                <div class="mr-14 w-auto">
+                <div class="w-auto mr-14">
                     <a href="/">
                         <x-logo class="h-8"></x-logo>
                     </a>
                 </div>
                 <div class="hidden w-auto lg:block">
                     <ul class="flex items-center mr-16">
-                        <li class="mr-9 font-medium hover:text-gray-700"><a href="/">Home</a></li>
-                        <li class="mr-9 font-medium hover:text-gray-700"><a href="/blog">Blog</a></li>
-                        <li class="mr-9 font-medium hover:text-gray-700"><a href="#">Features</a></li>
+                        <li class="font-medium mr-9 hover:text-gray-700"><a href="/">Home</a></li>
+                        <li class="font-medium mr-9 hover:text-gray-700"><a href="/blog">Blog</a></li>
+                        <li class="font-medium mr-9 hover:text-gray-700"><a href="#">Features</a></li>
                         <li class="font-medium hover:text-gray-700"><a href="/pricing">Pricing</a></li>
                     </ul>
                 </div>
@@ -20,9 +20,9 @@
         <div class="w-auto">
             <div class="flex flex-wrap items-center">
                 @if(auth()->guest())
-                    <div class="hidden mr-5 w-auto lg:block">
+                    <div class="hidden w-auto mr-5 lg:block">
                         <div class="inline-block">
-                            <a href="/auth/login" class="px-5 py-3 w-full font-medium bg-transparent rounded-xl transition duration-200 ease-in-out hover:text-gray-700" type="button">Sign In</a>
+                            <a href="/auth/login" class="w-full px-5 py-3 font-medium transition duration-200 ease-in-out bg-transparent rounded-xl hover:text-gray-700" type="button">Sign In</a>
                         </div>
                     </div>
                     <div class="hidden w-auto lg:block">
@@ -38,32 +38,30 @@
                     </div>
                 @endif
                 <div class="w-auto lg:hidden">
-                    <button x-on:click="mobileNavOpen = !mobileNavOpen" class="text-white bg-gray-900 p-3">
+                    <button x-on:click="mobileNavOpen = !mobileNavOpen" class="p-3 text-white bg-gray-900">
                         <x-phosphor-list class="w-6 h-6" />
                     </button>
                 </div>
             </div>
         </div>
     </div>
-    <div :class="{'block': mobileNavOpen, 'hidden': !mobileNavOpen}" class="hidden fixed top-0 bottom-0 left-0 z-50 w-4/6 sm:max-w-xs">
+    <div :class="{'block': mobileNavOpen, 'hidden': !mobileNavOpen}" class="fixed top-0 bottom-0 left-0 z-50 hidden w-4/6 sm:max-w-xs">
         <div x-on:click="mobileNavOpen = !mobileNavOpen" class="fixed inset-0 bg-gray-800 opacity-80"></div>
-        <nav class="overflow-y-auto relative z-10 px-9 pt-8 h-full bg-white">
+        <nav class="relative z-10 h-full px-5 pt-5 overflow-y-auto bg-white">
             <div class="flex flex-wrap justify-between h-full">
-                <div class="w-full">
-                    <div class="flex justify-between items-center -m-2">
-                        <div class="p-2 w-auto">
-                            <a class="inline-block" href="#">
-                                <img src="flaro-assets/logos/flaro-logo-black.svg" alt="">
-                            </a>
-                        </div>
-                        <div class="p-2 w-auto">
+                <div class="w-full pt-px">
+                    <div class="flex items-center justify-between">
+                        <a class="inline-block" href="/">
+                            <x-logo class="h-8" />
+                        </a>
+                        <div class="w-auto p-2">
                             <button x-on:click="mobileNavOpen = !mobileNavOpen">
                                 <svg width="24" height="24" viewbox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M6 18L18 6M6 6L18 18" stroke="#111827" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path></svg>
                             </button>
                         </div>
                     </div>
                 </div>
-                <div class="flex flex-col justify-center py-16 w-full">
+                <div class="flex flex-col justify-center w-full px-3 py-16">
                     <ul>
                         <li class="mb-12"><a class="font-medium hover:text-gray-700" href="/">Home</a></li>
                         <li class="mb-12"><a class="font-medium hover:text-gray-700" href="/blog">Blog</a></li>
@@ -71,14 +69,14 @@
                         <li><a class="font-medium hover:text-gray-700" href="/pricing">Pricing</a></li>
                     </ul>
                 </div>
-                <div class="flex flex-col justify-end pb-8 w-full">
+                <div class="flex flex-col justify-end w-full pb-8">
                     @if(auth()->guest())
-                        <div class="flex flex-wrap space-y-3 w-full">
-                            <a href="/auth/login" class="px-5 block text-center bg-gray-100 py-3 w-full font-medium transition duration-200 ease-in-out hover:text-gray-700" type="button">Sign In</a>
-                            <a href="/auth/register" class="px-5 block text-center py-3 w-full font-semibold text-white bg-gray-900 transition duration-200 ease-in-out focus:ring focus:ring-gray-900 hover:bg-gray-900">Try 14 Days Free Trial</a>
+                        <div class="flex flex-wrap w-full space-y-3">
+                            <a href="/auth/login" class="block w-full px-5 py-3 font-medium text-center transition duration-200 ease-in-out bg-gray-100 hover:text-gray-700" type="button">Sign In</a>
+                            <a href="/auth/register" class="block w-full px-5 py-3 font-semibold text-center text-white transition duration-200 ease-in-out bg-gray-900 focus:ring focus:ring-gray-900 hover:bg-gray-900">Try 14 Days Free Trial</a>
                         </div>
                     @else
-                        <a href="/dashboard" class="px-5 block text-center py-3 w-full font-semibold text-white bg-gray-900 transition duration-200 ease-in-out focus:ring focus:ring-gray-900 hover:bg-gray-900">View Dashboard</a>
+                        <a href="/dashboard" class="block w-full px-5 py-3 font-semibold text-center text-white transition duration-200 ease-in-out bg-gray-900 focus:ring focus:ring-gray-900 hover:bg-gray-900">View Dashboard</a>
                     @endif
                 </div>
             </div>
